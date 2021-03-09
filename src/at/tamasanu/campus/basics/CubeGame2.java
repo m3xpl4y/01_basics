fffc7967b2eae0c85adaf80b4cce047cc931211a
m3xpl4y <m3xpl4y@gmail.com>
2021-03-09 08:33:30 +0100

    Game win function corrected if Statement repaired

---
+++ src/at/tamasanu/campus/basics/CubeGame2.java
@@ -0,0 +1,83 @@
+package at.tamasanu.campus.basics;
+
+import javax.swing.*;
+import java.util.Random;
+import java.util.Scanner;
+
+public class CubeGame2 {
+
+    private static String playerName;
+    private static int playerOne = 0;
+    private static int playerTwo = 0;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Scanner scanName = new Scanner(System.in);
+        System.out.println("************************************");
+        System.out.println("           Würfelspiel");
+        System.out.println("************************************");
+        System.out.println("Geben Sie Ihren Namen ein:");
+        playerName = scanName.nextLine();
+        System.out.println("Hallo " + playerName);
+        while (true) {
+            cube_menu();
+            int mySelection = sc.nextInt();
+
+            switch (mySelection) {
+                case 1: Game();
+                    break;
+                case 2:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Bitte einen gültigen Menüpunkt auswählen");
+                    break;
+            }
+        }
+    }
+    private static void Game()
+    {
+        Random random = new Random();
+        int playerOneSum = 0;
+        int playerTwoSum = 0;
+
+        for (int i = 0; i < 6; i++) {
+            playerOne = random.nextInt(6 - 0) + 1;
+            playerOneSum = playerOneSum + playerOne;
+            //System.out.println(playerOne);
+        }
+        System.out.println("Der Computer hat die Augensumme " + playerOneSum);
+
+        for (int i = 0; i <= 6; i++) {
+            playerTwo = random.nextInt(6 - 0) + 1;
+            playerTwoSum = playerTwoSum + playerTwo;
+        }
+        System.out.println("Die Augensumme der Würfel von "+ playerName+ " ist " +playerTwoSum);
+
+        if(playerOneSum == playerTwoSum)
+        {
+            System.out.println("Spiel ist unentschieden!");
+        }
+        else if(playerTwoSum > playerOneSum)
+        {
+                System.out.println(playerName + " hat Gewonnen!");
+        }
+        else if(playerTwoSum < playerOneSum)
+        {
+                System.out.println("Computer hat gewonnen!");
+        }
+
+    }
+    private static void cube_menu()
+    {
+        String[] menu = {"1. Spiel Starten", "2. Beenden"};
+        System.out.println("Wähle aus folgenden Möglichkeiten:");
+
+        for (int i = 0; i < menu.length; i++)
+        {
+            System.out.println("----------------------------------");
+            System.out.println(menu[i]);
+        }
+    }
+}
