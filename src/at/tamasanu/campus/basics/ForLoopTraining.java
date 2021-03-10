8d8be7ae2290c2952fbfcf112a2e03d35a3d26eb
m3xpl4y <m3xpl4y@gmail.com>
2021-03-10 17:39:29 +0100

    Aufgabe 1

---
+++ src/at/tamasanu/campus/basics/ForLoopTraining.java
@@ -0,0 +1,12 @@
+package at.tamasanu.campus.basics;
+
+public class ForLoopTraining {
+    public static void main(String[] args) {
+        // i wird als integer deklariert und die zahl 1 zugewiesen
+        // so lange i kleiner oder gleich groß als 10 ist wird das so viele male ausgegeben
+        // i++ bedeutet das i + 1 ist. und wenn i 12 ist dann 12+1
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
