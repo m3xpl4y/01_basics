7a6d3abee88101ff2741fa03d3188ce40390042f
m3xpl4y <m3xpl4y@gmail.com>
2021-03-12 23:48:56 +0100

    ArraysAufgabe3 - sortierung der ausgegebenen Arrays

---
+++ src/at/tamasanu/campus/basics/ArraysAufgabe3Sorting.java
@@ -0,0 +1,26 @@
+package at.tamasanu.campus.basics;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class ArraysAufgabe3Sorting {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        int[] intArray = new int[10];
+        int[] intArray2 = new int[10];
+
+        for (int i = 0; i < intArray.length; i++) {
+            int rndNumb = rnd.nextInt(45);
+            intArray[i] = rndNumb;
+            System.out.println(intArray[i]);
+        }
+        //Möglichkeit 1:
+        Arrays.sort(intArray);
+        System.out.println(Arrays.toString(intArray));
+        //Möglichkeit 2:
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+
+    }
+}
