76305aeff53bded3253d60fcc2a80e215bc8e06a
m3xpl4y <m3xpl4y@gmail.com>
2021-03-17 17:12:59 +0100

    method calculations - created

---
+++ src/at/tamasanu/campus/classes/MethodCalculations.java
@@ -0,0 +1,25 @@
+package at.tamasanu.campus.classes;
+
+public class MethodCalculations {
+
+    public static double Subtraction(double numberOne, double numberTwo)
+    {
+        double result = 0;
+        result = numberOne - numberTwo;
+        return result;
+    }
+    public static double Multiplication(double numberOne, double numberTwo)
+    {
+        double result = 0;
+        result = numberOne * numberTwo;
+        return result;
+    }
+    public static double Division(double numberOne, double numberTwo)
+    {
+        double result = 0;
+        result = numberOne / numberTwo;
+        return result;
+    }
+
+
+}
