1da98906c89d061fb99aa1564e19872930143366
m3xpl4y <m3xpl4y@gmail.com>
2021-03-22 18:36:14 +0100

    reviewed and revised

--- src/at/tamasanu/campus/classes/guessWordsClasses.java
+++ src/at/tamasanu/campus/classes/guessWordsClasses.java
@@ -11,5 +11,16 @@ public class guessWordsClasses {
         int index = rnd.nextInt(array.length);
         return array[index];
     }
+    public static void guessWordsMenu()
+    {
+        String[] menu = {"1. Spiel starten", "2. Beenden"};
+        System.out.println("             Bitte wählen:");
+
+        for (int i = 0; i < menu.length; i++)
+        {
+            System.out.println(menu[i]);
+            System.out.println("------------------------------");
+        }
+    }
 
 }
