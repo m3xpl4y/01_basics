5e867e6f3402cec3b4a0b4d2755c9b16e861688c
m3xpl4y <m3xpl4y@gmail.com>
2021-03-24 17:31:37 +0100

    reviewed and revised

--- src/at/tamasanu/campus/games/guessWords.java
+++ src/at/tamasanu/campus/games/guessWords.java
@@ -13,6 +13,8 @@ public class guessWords {
     static String choosenWord = guessWordsClasses.getRandomWord(rndWordArray);
     static String[] stringChoosenWord = new String[choosenWord.length()];
     static String input = "";
+    static String[] charString = new String[choosenWord.length()];
+    static String toLowerCaseChosenWord = choosenWord.toLowerCase();
 
     public static void main(String[] args) {
 
@@ -25,28 +27,13 @@ public class guessWords {
         switch (mySelection)
         {
             case 1:
-                String[] charString = new String[choosenWord.length()];
-                String toLowerCaseChosenWord = choosenWord.toLowerCase();
-                for (int i = 0; i < choosenWord.length(); i++) {
-                    stringChoosenWord[i] = Character.toString(toLowerCaseChosenWord.charAt(i));
-                }
-                String starStr = "*";
-                for (int i = 0; i < choosenWord.length(); i++) {
-                    charString[i] = starStr;
-                }
-                while(true) {
-
-                System.out.println(choosenWord); //Gibt ein Zufälliges Wort aus dem Array aus.
+                firstWordLoops();
+                while(true)
+                {
+                System.out.println(choosenWord); //Gibt ein Zufälliges Wort aus dem Array aus. Kommetieren um das gesuchte Wort nicht mehr anzuzeigen!
 
                 System.out.println("Bitte einen Buchstaben auswählen");
-                input = scn.nextLine();
-                    if (String.join("", charString).contains("*")) {
-                        for (int i = 0; i < choosenWord.length(); i++) {
-                            if (Character.toString(choosenWord.charAt(i)).equalsIgnoreCase(input)) {
-                                charString[i] = input;
-                            }
-                        }
-                    }
+                mainGame();
                     if(Arrays.asList(stringChoosenWord).contains(input))
                     {
                         System.out.println("Buchstabe " + input.toUpperCase() + " ist vorhanden");
@@ -55,19 +42,39 @@ public class guessWords {
                     {
                         System.out.println("Buchstabe " + input.toUpperCase() + " ist NICHT vorhanden");
                     }
-                if(choosenWord.equalsIgnoreCase(String.join("",charString)))
-                {
-                    System.out.println("Sie haben gewonnen");
-                    break;
+                    if(choosenWord.equalsIgnoreCase(String.join("",charString)))
+                    {
+                        System.out.println("Sie haben gewonnen");
+                        break;
+                    }
                 }
-                System.out.println("Result, Buchstabe mit Sterne: " + String.join("",charString));
-            }
-
                 break;
             case 2: MainMenu.main(args);
                 break;
             default:
                 System.out.println("Echt jetzt?");
         }
+    }
+    static void firstWordLoops()
+    {
+        for (int i = 0; i < choosenWord.length(); i++) {
+            stringChoosenWord[i] = Character.toString(toLowerCaseChosenWord.charAt(i));
+        }
+        String starStr = "*";
+        for (int i = 0; i < choosenWord.length(); i++) {
+            charString[i] = starStr;
+        }
+    }
+    static void mainGame()
+    {
+        input = scn.nextLine();
+        if (String.join("", charString).contains("*")) {
+            for (int i = 0; i < choosenWord.length(); i++) {
+                if (Character.toString(choosenWord.charAt(i)).equalsIgnoreCase(input)) {
+                    charString[i] = input;
+                }
+            }
+        }
+        System.out.println("Result, Buchstabe mit Sterne: " + String.join("",charString));
     }
 }
